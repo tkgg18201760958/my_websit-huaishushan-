eaf258769882b0c5b337eb0a0cfae044f47d30f1
chengyi <tkgg711720@outlook.com>
2024-11-02 18:01:42 +0800

    学习js函数

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("Hello World");
 alert("Hello World");
 for (let i = 0; i < 10; i++) { 
-    alert(i);
+    console.log(i);
+    log_print_hello();
+}
+function log_print_hello(){
+    console.log("Hello World");
 }
