58f3291a24bce444c7606e791a5eaea01a28a2e0
chengyi <tkgg711720@outlook.com>
2024-11-02 22:08:10 +0800

    使用CSS美化

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let mouseclick = 0;
 console.log("open");
 
 window.onload = function() {
-    var id = document.getElementById("click_time");
+    let id = document.getElementById("click_time");
     console.log(id);
     id.innerHTML = mouseclick;
 
