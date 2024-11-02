a02172b5e33412bf9a1094d1ae5c0f89d428e5dc
chengyi <tkgg711720@outlook.com>
2024-11-02 22:00:41 +0800

    引用style.css和script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let mouseclick = 0;
 console.log("open");
-window.onload=function(){
+
+window.onload = function() {
     var id = document.getElementById("click_time");
     console.log(id);
-    id.innerHTML = mouseclick;    
-}
-function mouseover(){
-    console.log(mouseclick)
-    mouseclick++;
+    id.innerHTML = mouseclick;
+
+    var button = document.getElementById("click_button");
+    button.addEventListener("click", function() {
+        mouseclick++;
+        id.innerHTML = mouseclick;
+    });
 }
