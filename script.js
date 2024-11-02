082aa844c6159a5a92ec0ea6bb71a9c5462c15cf
chengyi <tkgg711720@outlook.com>
2024-11-02 18:26:34 +0800

    学习getElementById函数

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hello World");
 alert("Hello World");
+get_id("二级标签")
 for (let i = 0; i < 101; i++) { 
     time_stop(i);
     console.log(i);
@@ -9,4 +10,8 @@ function time_stop(time){
         console.log("打印完成100次");
         return "打印完成100次";
     }
+}
+function get_id(id){
+    let id = document.getElementById(id);
+    console.log(id);
 }
