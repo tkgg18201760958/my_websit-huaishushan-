2b193c90ec99cc316e91a1fb067538e0f6ec940c
chengyi <tkgg711720@outlook.com>
2024-11-02 21:24:17 +0800

    成功解决DOM问题

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-console.log("open");
-var id = document.getElementById("click_time");
-console.log(id);
-let mouseclick = 0;
-function mouseover(){
-    console.log(mouseclick)
-    mouseclick++;
+window.onload=function(){
+    console.log("open");
+        var id = document.getElementById("click_time");
+        console.log(id);
+        let mouseclick = 0;
+        function mouseover(){
+        console.log(mouseclick)
+        mouseclick++;
+    }
+    console.log("over")
 }
-console.log("over")
