bdb51699385a2180559c75aa7f32b0bcdf076c52
chengyi <tkgg711720@outlook.com>
2024-11-03 09:03:15 +0800

    修复点击无法显示的问题

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let mouseclick = 0;
 console.log("open");
 window.onload=function(){
-    var id = document.getElementById("click_time");
+    let id = document.getElementById("click_time");
     console.log(id);
-    id.innerHTML = mouseclick;    
-}
-function mouseover(){
-    console.log(mouseclick)
-    mouseclick++;
+    id.innerHTML = mouseclick;
+
+    let button = document.getElementById("button_hello");
+    button.addEventListener("click", function() {
+        mouseclick++;
+        id.innerHTML = mouseclick;
+        console.log(mouseclick);
+    });
 }
